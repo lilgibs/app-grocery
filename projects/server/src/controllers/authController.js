9a3cd184c67608ba6037790bf235dccc02868b89
Ra <88369275+callraV@users.noreply.github.com>
2023-06-05 20:37:07 +0800

    admin login token

--- projects/server/src/controllers/authController.js
+++ projects/server/src/controllers/authController.js
@@ -12,6 +12,12 @@ module.exports = {
         return res.status(400).send({ message: "You are not registered as an admin" });
       }
 
+      // const isValid = await bcrypt.compare(password, isEmailExist[0].password);
+
+      // if (!isValid) {
+      //   return res.status(400).send({ message: "Email or Password is incorrect" });
+      // }
+
       let payload = { adminId: isEmailExist[0].admin_id };
 
       const token = jwt.sign(payload, "joe", { expiresIn: "1h" });
@@ -22,6 +28,27 @@ module.exports = {
         data: { id: isEmailExist[0].admin_id, name: isEmailExist[0].name, email: isEmailExist[0].email, name: isEmailExist[0].name, role: isEmailExist[0].role, store_id: isEmailExist[0].store_id, is_deleted: isEmailExist[0].is_deleted },
       });
       // return res.status(200).send({ message: "Admin login test" });
+    } catch (error) {
+      res.status(error.status || 500).send(error);
+    }
+  },
+  checkAdminLogin: async (req, res) => {
+    try {
+      //return res.status(200).send(req.body);
+      const { email, password } = req.body;
+      const admin = await query(`SELECT * FROM admins WHERE email=${db.escape(email)}`);
+
+      if (admin[0].role == 0) {
+        return res.status(200).send({
+          message: "Super admin verified",
+          data: { id: admin[0].admin_id, name: admin[0].name, email: admin[0].email, name: admin[0].name, role: admin[0].role, store_id: admin[0].store_id, is_deleted: admin[0].is_deleted },
+        });
+      }
+
+      return res.status(200).send({
+        message: "Branch admin verified",
+        data: { id: admin[0].admin_id, name: admin[0].name, email: admin[0].email, name: admin[0].name, role: admin[0].role, store_id: admin[0].store_id, is_deleted: admin[0].is_deleted },
+      });
     } catch (error) {
       res.status(error.status || 500).send(error);
     }
--- projects/server/src/routes/authRoutes.js
+++ projects/server/src/routes/authRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const { authController } = require("../controllers");
+const { verifyToken } = require("../middleware/auth");
 
 const router = express.Router();
 
 router.post("/adminlogin", authController.adminLogin);
+router.post("/check-adminlogin", verifyToken, authController.checkAdminLogin);
 
 module.exports = router;
