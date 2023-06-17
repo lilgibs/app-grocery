2d157f882f88290027b069456b7f5fff21a6bde5
lilgibs <lil.gibran01@gmail.com>
2023-06-17 23:26:27 +0700

    Feat: Admin Product List Page

--- projects/server/src/routes/admin/adminProductRoutes.js
+++ projects/server/src/routes/admin/adminProductRoutes.js
@@ -2,16 +2,19 @@ const express = require('express')
 const { adminProductController } = require('../../controllers')
 const { check } = require('express-validator')
 const upload = require('../../middleware/uploadMiddleware')
+const { adminVerifyToken } = require('../../middleware/adminAuth')
 const router = express.Router()
 
-router.get('/', adminProductController.getProductByName)
+router.get('/', adminProductController.getProducts)
+router.get('/inventory', adminVerifyToken, adminProductController.getStoreProducts)
+router.get('/:productId', adminProductController.getProductById)
 router.post('/add-product',
   upload.array('product_images', 3),adminProductController.addProduct)
-router.put('/:productID',
+router.put('/:productId',
   upload.single('product_category_image'),
   check('product_category_name').notEmpty().withMessage('Category name is required'),
   adminProductController.updateProduct)
-router.delete('/:productID', adminProductController.deleteProduct)
-router.delete('/:productID/permanently', adminProductController.hardDeleteProduct)
+router.delete('/:productId', adminVerifyToken, adminProductController.deleteProduct)
+router.delete('/:productId/permanently', adminProductController.hardDeleteProduct)
 
 module.exports = router
