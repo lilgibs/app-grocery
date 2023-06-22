db4f0a38b3e76e1cadc2cd26d3cd2b3522fa6fe2
lilgibs <lil.gibran01@gmail.com>
2023-06-22 20:22:27 +0700

    Feat: Added basic funcionallity for product detail

--- projects/server/src/controllers/productController.js
+++ projects/server/src/controllers/productController.js
@@ -5,7 +5,6 @@ const { handleServerError } = require("../utils/errorHandlers");
 module.exports = {
   getProducts: async (req, res, next) => {
     let storeId = req.query.storeId
-    
 
     if (!storeId) {
       const defaultStoreQuery = 'SELECT store_id FROM stores LIMIT 1';
@@ -20,6 +19,7 @@ module.exports = {
           p.product_name,
           p.product_description,
           p.product_price,
+          si.store_inventory_id,
           si.quantity_in_stock,
           si.store_id,
           (SELECT image_url FROM product_images WHERE product_id = p.product_id LIMIT 1) as image_url
@@ -42,7 +42,7 @@ module.exports = {
     const { productId } = req.params;
     let storeId = req.query.storeId;
 
-    const errors = validationResult(req);  
+    const errors = validationResult(req);
 
     if (!storeId) {
       const defaultStoreQuery = 'SELECT store_id FROM stores LIMIT 1';
@@ -68,6 +68,7 @@ module.exports = {
           product_name: productResult[0].product_name,
           product_description: productResult[0].product_description,
           product_price: productResult[0].product_price,
+          store_inventory_id: storeInventoryResult[0].store_inventory_id,
           quantity_in_stock: storeInventoryResult[0].quantity_in_stock,
           store_id: storeInventoryResult[0].store_id,
           product_images: imageResult,
@@ -80,7 +81,7 @@ module.exports = {
       } else {
         throw {
           status_code: 404,
-          message: "Product not found!",
+          message: "We apologize, but the product is not available in your area.",
         }
       }
     } catch (error) {
