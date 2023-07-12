6c253fa9ffec5f89d4ce7d37b94975ea1ab28a7b
lilgibs <lil.gibran01@gmail.com>
2023-07-12 15:48:13 +0700

    Update: Incorporating a 'Product Weight' column on the 'Add Product' page, and segregating API functionalities.

--- projects/server/src/config/db.js
+++ projects/server/src/config/db.js
@@ -2,11 +2,11 @@ const mysql = require("mysql2");
 const util = require("util");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "db_jcwdol0907",
-  port: 3306,
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  port: process.env.DATABASE_PORT,
 });
 
 db.connect((err) => {
