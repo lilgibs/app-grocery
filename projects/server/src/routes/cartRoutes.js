fad3b1bb0dce4ff3eb67e06ecb4aa782d7ff5792
Ra <88369275+callraV@users.noreply.github.com>
2023-07-17 16:47:11 +0800

    user can use vouchers

--- projects/server/src/routes/cartRoutes.js
+++ projects/server/src/routes/cartRoutes.js
@@ -4,6 +4,7 @@ const { shippingController } = require("../controllers");
 const router = express.Router();
 
 router.get("/", cartController.getCart);
+router.get("/voucher", cartController.getVoucher);
 router.post("/", cartController.addToCart);
 router.delete("/", cartController.deleteFromCart);
 router.patch("/", cartController.updateCart);
