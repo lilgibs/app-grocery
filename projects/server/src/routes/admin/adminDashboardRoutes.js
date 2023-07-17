26ac111ef59fb6ad440551e774da9fc7cbf124cd
lilgibs <lil.gibran01@gmail.com>
2023-07-17 18:42:24 +0700

    Update: Fix bug for user count, and product sold Admin Dashboard
    OG-53

--- projects/server/src/routes/admin/adminDashboardRoutes.js
+++ projects/server/src/routes/admin/adminDashboardRoutes.js
@@ -6,8 +6,9 @@ const router = express.Router()
 
 router.get('/daily-sales', adminVerifyToken, adminDashboardController.getDailySales)
 router.get('/monthly-sales', adminVerifyToken, adminDashboardController.getMonthlySales)
-router.get('/stores', adminDashboardController.getBranchStores)
-router.get('/products', adminDashboardController.getProducts)
+router.get('/stores', adminVerifyToken, adminDashboardController.getBranchStores)
+router.get('/users', adminVerifyToken, adminDashboardController.getUsers)
+router.get('/products', adminVerifyToken, adminDashboardController.getProducts)
 router.get('/products-sold', adminVerifyToken, adminDashboardController.getProductsSold)
 
 module.exports = router
